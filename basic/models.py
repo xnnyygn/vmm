d33d6de8d37a1762cd57842cc01a558af8b18875
xnnyygn <xnnyygn@gmail.com>
2014-03-06 10:38:53 +0800

    convert tab to 4 spaces

--- basic/models.py
+++ basic/models.py
@@ -2,30 +2,30 @@ from django.db import models
 from django.contrib import admin
 
 class Application(models.Model):
-	# just user name
-	proposer = models.CharField(max_length = 255)
-	date_applied = models.DateTimeField(auto_now = True)
-        status = models.CharField(max_length = 16)
+    # just user name
+    proposer = models.CharField(max_length = 255)
+    date_applied = models.DateTimeField(auto_now = True)
+    status = models.CharField(max_length = 16)
 
 class MachineTemplate(models.Model):
-	name = models.CharField(max_length = 255)
-	description = models.TextField()
-	total = models.IntegerField()
-	remaining = models.IntegerField()
+    name = models.CharField(max_length = 255)
+    description = models.TextField()
+    total = models.IntegerField()
+    remaining = models.IntegerField()
 
 class MachineTemplateAdmin(admin.ModelAdmin):
-	list_display = ('name', 'total', 'remaining')
+    list_display = ('name', 'total', 'remaining')
 
 class ApplicationDetail(models.Model):
-	application = models.ForeignKey(Application)
-	template = models.ForeignKey(MachineTemplate)
-	count = models.IntegerField()
+    application = models.ForeignKey(Application)
+    template = models.ForeignKey(MachineTemplate)
+    count = models.IntegerField()
 
 class MachineUsage(models.Model):
-	application = models.ForeignKey(Application)
-	date_actived = models.DateTimeField(auto_now = True)
-	due_date = models.DateTimeField()
-	hostname = models.CharField(max_length = 255)
-	ip = models.CharField(max_length = 64)
+    application = models.ForeignKey(Application)
+    date_actived = models.DateTimeField(auto_now = True)
+    due_date = models.DateTimeField()
+    hostname = models.CharField(max_length = 255)
+    ip = models.CharField(max_length = 64)
 
 admin.site.register(MachineTemplate, MachineTemplateAdmin)
--- vmm/settings.py
+++ vmm/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -83,5 +82,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-		BASE_DIR + '/templates',
+    BASE_DIR + '/templates',
 )
